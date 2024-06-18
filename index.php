4303ff9a4a3c3ad06c15e414186f5ce324b2301e
liamRouaz <liam.rouaz@laplateforme.io>
2024-06-18 15:44:40 +0200

    instalation de bootstrap jquery et poopers js

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQ6dfh7/j5Z5ro5i+S4eGA6WmgLaP2VtM3Pb9czox9YfX3" crossorigin="anonymous">
 </head>
 <body>
-    
+
+  
+
+ 
 </body>
+ <!-- Bootstrap JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldH5pa31gD2A1BZpXK6R9vVO7oKJgSpEdU5i1Gw" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgSLI2Q9KtLkvJRa4BJdN1eH8OkqKpG5DVuzqGjq5w0b8acdx2F" crossorigin="anonymous"></script>
 </html>
