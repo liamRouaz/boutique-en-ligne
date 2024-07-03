00c169a79c82a607df13a59af8b837fef73c45b8
liamRouaz <liam.rouaz@laplateforme.io>
2024-07-04 01:50:07 +0200

    doctype pour signin

---
+++ singin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="email">Email</label>
+        <input type="email" placeholder="Entrez votre mail" id="email" name="email" required />
+        <label for="password">Mots de passe</label>
+        <input type="password" placeholder="Entrez votre mots de passe" id="password" name="password" required />
+        <input type="submit" value="se connecter" name="ok">
+    </form>
+</body>
+</html>
