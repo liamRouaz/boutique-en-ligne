ddc91e7b408c6b6ec742d2189834c528d1e18768
liamRouaz <liam.rouaz@laplateforme.io>
2024-07-04 02:18:10 +0200

    creation du formulaire d'inscription

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inscription</title>
+</head>
+<body>
+    <style>
+        input{
+            margin-bottom: 10px;
+        }
+    </style>
+    <!-- name reference des variable -->
+    <form method="POST" action="traitement.php">
+        <label for="name">Nom</label>
+        <input type="text" placeholder="Entrez votre nom" name="nom" id="nom">
+        <br>
+        <label for="">Prenom</label>
+        <input type="text" placeholder="Entrez votre prenom" name="prenom" id="prenom">
+        <br>
+        <label for="">Pseudo</label>
+        <input type="text" placeholder="Entrez votre pseudo" name="pseudo" id="pseudo">
+        <br>
+        <label for="email">Email</label>
+        <input type="email" placeholder="Entrez votre mail" id="email" name="email" required />
+        <br>
+        <label for="password">Mots de passe</label>
+        <input type="password" placeholder="Entrez votre mots de passe" id="password" name="password" required />
+        <input type="submit" value="m'inscricre" name="ok">
+    </form>
+</body>
+</html>
---
+++ traitement.php
@@ -0,0 +1,10 @@
+<?php 
+if(isset($_POST["ok"])){
+    //extract pour créé une variable apartire d'un name 
+    extract($_POST);
+    echo $nom=$_POST['nom']; 
+    echo $prenom=$_POST['prenom'];
+    echo $pseudo=$_POST['pseudo'];
+    echo $mdp=$_POST['password'];
+    echo $mail=$_POST['email'];
+}?>
